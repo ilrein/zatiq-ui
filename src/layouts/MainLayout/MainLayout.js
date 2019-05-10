e0f72544af4eff22cfdd6cfb6f6cc3d766e25c3b
Ilia <ilia@bookjane.com>
2019-05-10 10:23:55 -0400

    visible on sidebar pusher only acts when its open

--- src/layouts/MainLayout/MainLayout.js
+++ src/layouts/MainLayout/MainLayout.js
@@ -65,7 +65,7 @@ const MainLayout = ({ history, children }) => {
 
               <Sidebar.Pusher
                 dimmed={visible}
-                onClick={toggleVisible}
+                onClick={visible ? toggleVisible : null}
               >
                 <>
                   <Navbar
