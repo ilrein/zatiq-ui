2224aab4ff0b7240259268df8a2a17f1dc77e573
Ilia Reingold <iliareingold@Ilias-MacBook-Pro.local>
2019-05-18 12:53:09 -0400

    updated boilerplate for dishes page

--- src/pages/Items/Items.js
+++ src/pages/Items/Items.js
@@ -1,9 +1,55 @@
-import React from 'react';
+/* eslint-disable */
 
-const Items = () => (
-  <div>
-    Items
-  </div>
-);
+import React, { useState } from 'react';
+import {
+  Form,
+  Button,
+} from 'semantic-ui-react';
+import styled from 'styled-components';
+import fetch from 'isomorphic-fetch';
+import { toast } from 'react-toastify';
+import PropTypes from 'prop-types';
+
+import fadeIn from '../../anime/fadeIn';
+import {
+  API_COMPANY,
+} from '../../constants';
+
+const Wrapper = styled.div`
+  display: flex;
+  animation: ${fadeIn} 1s ease;
+`;
+
+const InnerWrapper = styled.div`
+  margin: 0.25rem 1rem;
+  width: 100%;
+`;
+
+const Items = ({
+  // userReducer,
+  // company,
+  // captureCompany,
+}) => {
+  // const [name, setName] = useState(company.name);
+  // const { cognitoUser } = userReducer;
+
+  // const token = cognitoUser.signInUserSession.idToken.jwtToken;
+
+  // const [saving, setSaving] = useState(false);
+
+  return (
+    <Wrapper>
+      <InnerWrapper>
+        Items page.
+      </InnerWrapper>
+    </Wrapper>
+  );
+};
+
+Items.propTypes = {
+  userReducer: PropTypes.shape().isRequired,
+  company: PropTypes.shape().isRequired,
+  captureCompany: PropTypes.func.isRequired,
+};
 
 export default Items;
