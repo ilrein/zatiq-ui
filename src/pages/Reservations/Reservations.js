0622ed23a9183c75d4048cd179d2dcc739a1c859
Ilia Reingold <iliareingold@Ilias-MacBook-Pro.local>
2019-05-18 12:52:17 -0400

    quick reservation component basic styling

--- src/pages/Reservations/Reservations.js
+++ src/pages/Reservations/Reservations.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Form,
+  Button,
+} from 'semantic-ui-react';
+import styled from 'styled-components';
+import fetch from 'isomorphic-fetch';
+import { toast } from 'react-toastify';
+import PropTypes from 'prop-types';
 
-const Reservations = () => (
-  <div>
-    Reservations
-  </div>
-)
+import fadeIn from '../../anime/fadeIn';
+import {
+  API_COMPANY,
+} from '../../constants';
+
+const Wrapper = styled.div`
+  display: flex;
+  animation: ${fadeIn} 1s ease;
+`;
+
+const InnerWrapper = styled.div`
+  margin: 0.25rem 1rem;
+  width: 100%;
+`;
+
+/* eslint-disable */
+const Reservations = ({
+  // userReducer,
+  // company,
+  // captureCompany,
+}) => {
+  // const [name, setName] = useState(company.name);
+  // const { cognitoUser } = userReducer;
+
+  // const token = cognitoUser.signInUserSession.idToken.jwtToken;
+
+  // const [saving, setSaving] = useState(false);
+
+  return (
+    <Wrapper>
+      <InnerWrapper>
+        Coming soon.
+      </InnerWrapper>
+    </Wrapper>
+  );
+};
+
+Reservations.propTypes = {
+  userReducer: PropTypes.shape().isRequired,
+  company: PropTypes.shape().isRequired,
+  captureCompany: PropTypes.func.isRequired,
+};
 
 export default Reservations;
