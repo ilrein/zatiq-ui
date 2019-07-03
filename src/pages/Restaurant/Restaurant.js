ce807b271ad10f133125721f1a61191b03bdad09
Ilia <ilia@bookjane.com>
2019-07-03 00:53:35 -0400

    setting up restaurant edit page

--- src/pages/Restaurant/Restaurant.js
+++ src/pages/Restaurant/Restaurant.js
@@ -3,6 +3,7 @@ import {
   Form,
   Button,
   Header,
+  Tab,
 } from 'semantic-ui-react';
 import styled from 'styled-components';
 import fetch from 'isomorphic-fetch';
@@ -13,6 +14,9 @@ import fadeIn from '../../anime/fadeIn';
 import {
   API_RESTAURANT,
 } from '../../constants';
+import PlacesAutoComplete from '../../components/PlacesAutoComplete';
+import CuisineDropdown from '../../components/CuisineDropdown';
+import FeaturesDropdown from '../../components/FeaturesDropdown';
 
 const Wrapper = styled.div`
   display: flex;
@@ -29,12 +33,32 @@ const Restaurant = ({
   restaurant,
   captureRestaurant,
 }) => {
+  // restaurant stuff
   const [name, setName] = useState(restaurant.name);
-  const { cognitoUser } = userReducer;
+  const [address, setAddress] = useState(restaurant.address);
+  const [description, setDescription] = useState(restaurant.description);
+  const [cuisineType, setCuisineType] = useState(restaurant.cuisine);
+  // const [image, setImage] = useState(restaurant.image);
+  const [phone, setPhone] = useState(restaurant.phone);
 
-  const token = cognitoUser.signInUserSession.idToken.jwtToken;
+  // features
+  const [features, setFeatures] = useState(restaurant.features);
 
+  // prices
+  const [minPrice, setMinPrice] = useState(restaurant.priceRangeMin);
+  const [maxPrice, setMaxPrice] = useState(restaurant.priceRangeMax);
+
+  // start/close times
+  const [startingTime, setStartingTime] = useState(restaurant.startingTime);
+  const [closingTime, setClosingTime] = useState(restaurant.closingTime);
+
+  // update states
+  const [loading, setLoading] = useState(false);
   const [saving, setSaving] = useState(false);
+
+  // tokens
+  const { cognitoUser } = userReducer;
+  const token = cognitoUser.signInUserSession.idToken.jwtToken;
 
   const onSubmit = async () => {
     setSaving(true);
@@ -62,35 +86,96 @@ const Restaurant = ({
     }
   };
 
+  const Panes = [
+    {
+      menuItem: 'Details',
+      render: () => (
+        <Tab.Pane>
+          <Form>
+            <Form.Field>
+              <label>
+                Name
+              </label>
+              <Form.Input
+                placeholder="Tesla Inc."
+                value={name}
+                onChange={(event, { value }) => setName(value)}
+                fluid
+              />
+            </Form.Field>
+  
+            <div className="field">
+              <label>
+                Address
+              </label>
+              <PlacesAutoComplete
+                onPlaceSelected={place => setAddress(place)}
+                placeholder={address}
+              />
+            </div>
+  
+            <Form.TextArea
+              onChange={(event, { value }) => setDescription(value)}
+              value={description}
+              label="Description"
+              placeholder="Restaurant description here..."
+              disabled={loading}
+              required
+            />
+  
+            <div className="field required">
+              <label>
+                Cuisine Type
+              </label>
+              <CuisineDropdown
+                onChange={(event, { value }) => setCuisineType(value)}
+                fluid
+                required
+                disabled={loading}
+                value={cuisineType}
+              />
+            </div>
+  
+            <div className="field">
+              <label>
+                Features
+              </label>
+              <FeaturesDropdown
+                onChange={(event, { value }) => setFeatures(value)}
+                fluid
+                required
+                disabled={loading}
+                value={features}
+              />
+            </div>
+  
+            <Button
+              type="submit"
+              primary
+              onClick={onSubmit}
+              loading={saving}
+            >
+              Submit
+            </Button>
+          </Form>
+        </Tab.Pane>
+      ),
+    },
+    {
+      menuItem: 'Payment',
+      render: () => <Tab.Pane>Payment details</Tab.Pane>
+    },
+  ];
+
   return (
     <Wrapper>
       <InnerWrapper>
         <Header>
-          Edit
-          &nbsp;
           {restaurant.name}
         </Header>
-        <Form>
-          <Form.Field>
-            <label>
-              Name
-            </label>
-            <Form.Input
-              placeholder="Tesla Inc."
-              value={name}
-              onChange={(event, { value }) => setName(value)}
-              fluid
-            />
-          </Form.Field>
-          <Button
-            type="submit"
-            primary
-            onClick={onSubmit}
-            loading={saving}
-          >
-            Submit
-          </Button>
-        </Form>
+        <Tab
+          panes={Panes}
+        />
       </InnerWrapper>
     </Wrapper>
   );
