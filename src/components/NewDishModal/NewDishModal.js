10f4c0088e1825ac7293d503ba4eeaa47c9749dc
Ilia <ilia@bookjane.com>
2019-07-29 13:28:48 -0400

    fixe free addons bug

--- src/components/NewDishModal/NewDishModal.js
+++ src/components/NewDishModal/NewDishModal.js
@@ -323,7 +323,7 @@ const NewDishModal = ({
                   setFreeAddons(['']);
                   return;
                 }
-                setFreeAddons(null);
+                setFreeAddons([]);
               }}
               checked={hasFreeAddons}
             />
