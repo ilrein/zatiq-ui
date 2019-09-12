d1f4707d9c838cfbcbd9bb4bbfff2c51d0521513
Ilia <ilia@bookjane.com>
2019-09-12 19:26:27 -0400

    cahnged domain name

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -2,7 +2,7 @@
  * API_URL needs to be swapped when deploying to prod
  */
 
-// const API_URL = process.env.REACT_APP_API_URL_PROD;
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = process.env.REACT_APP_API_URL_PROD;
+// const API_URL = process.env.REACT_APP_API_URL;
 
 export default API_URL;
